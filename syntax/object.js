fbdaaeae68bf1dbf72613121a2974baf2588cdee
DavidLeehyungu <leencjr95@gmail.com>
2022-07-02 21:10:17 +0900

    End commit

--- syntax/object.js
+++ syntax/object.js
@@ -6,13 +6,19 @@ while(i < members.length){
   i = i + 1;
 }
 
+
+
 var roles = {
   'programmer':'egoing',
   'designer' : 'k8805',
   'manager' : 'hoya'
 }
+
 console.log(roles.designer);
 console.log(roles['designer']);
+
+
+
 for(var n in roles){
   console.log('object => ', n, 'value => ', roles[n]);
 }
